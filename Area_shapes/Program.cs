2290baadcf13f137f386bbd371590799a30482b3
AndreiOrzan <48299309+AndreiOrzan@users.noreply.github.com>
2019-07-09 21:17:17 +0100

    Initial commit

---
+++ Area_shapes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = { new Circle(5), new Rectangle(4, 5)};
+
+            foreach (var s in shapes)
+            {
+                s.GetInfo();
+                Console.WriteLine("{0} Area : {1:f2}", s.Name, s.Area());
+
+                Circle testCirc = s as Circle;
+                if (testCirc == null)
+                {
+                    Console.WriteLine("This is not a circle");
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Area_shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_shapes
+{
+    class Rectangle:Shape
+    {
+        public double Lenght { set; get; }
+        public double Width { set; get; }
+
+        public Rectangle(double lenght, double width)
+        {
+            Name = "Rectangle";
+            Lenght = lenght;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Lenght * Width; 
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"It has a lenght of {Lenght} and has a width of {Width} ");
+        }
+    }
+}
